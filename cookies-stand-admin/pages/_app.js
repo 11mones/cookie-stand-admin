e5062ca8f2c725dbf1b798dde2905dea7413eb27
11mones <mones_leader@hotmail.com>
2023-08-26 15:20:19 +0000

    ---

--- cookies-stand-admin/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '@/styles/globals.css'
-import { Authprovider } from '@/contexts/auth'
-export default function App({ Component, pageProps }) {
-
-  return (
-    <Authprovider >
-      <Component {...pageProps} />
-    </Authprovider>)
-}
